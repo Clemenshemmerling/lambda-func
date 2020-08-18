699e255e00a693dfd3898adb64e29b05ca1df79f
Clemens Hemmerling <clemens@getkor.io>
2020-08-17 20:16:36 -0600

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from KOR!'),
+        body: JSON.stringify('Hello from Jacobo!'),
     };
     return response;
 };
